bd8482438b2a2537339a17cb24b00bea75d12dda
igorkll <frerther@gmail.com>
2026-09-06 13:23:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,13 @@ if [ "$EUID" -ne 0 ]; then
   exec sudo "$0" "$@"
 fi
 
-apt install lua5.3
+echo "INSTALLER: apt update"
+apt update
+
+echo "INSTALLER: install system packages"
+apt install -y lua5.3
+
+echo "INSTALLER: install system cli tools"
 mkdir -m 755 -p /usr/local/bin
 cp -f "edit.lua" "/usr/local/bin/edit"
 chmod 755 "/usr/local/bin/edit"
